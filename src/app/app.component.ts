444ab5389c565ed20238ea881261e16adb469a1c
erind <erindmehmeti@gmail.com>
2023-02-13 18:43:01 +0100

    added card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      title: 'Neat Tree',
+      imageUrl:'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+
+    { 
+      title: 'Snow Mountain',
+      imageUrl:'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of snowy mountain'
+    },
+
+    { 
+      title: 'Mountain biking',
+      imageUrl:'assets/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some biking today'
+    }
+
+
+  ]
 }
